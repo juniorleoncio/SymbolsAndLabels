5c621468efa2fbfd66734717dda46413d53e55e6
Junior Leoncio <graciano.leoncio@gmail.com>
2024-03-29 23:50:22 -0300

    Initial Commit

---
+++ SymbolsAndLabels/SymbolsAndLabelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolsAndLabelsApp.swift
+//  SymbolsAndLabels
+//
+//  Created by Junior Leoncio on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SymbolsAndLabelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
